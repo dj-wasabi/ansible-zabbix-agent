45a768f3fc048f4904c3e6698a8a9e27ed0d926f
Werner Dijkerman <ikben@werner-dijkerman.nl>
2015-08-24 15:43:00 +0200

    Added tests and some extra vars for role installation

--- test/integration/default/serverspec/localhost/ansible_zabbix_agent_spec.rb
+++ test/integration/default/serverspec/localhost/ansible_zabbix_agent_spec.rb
@@ -7,10 +7,25 @@ describe 'Zabbix Agent Packages' do
     end
 end
 
+describe 'Zabbix Agent Services' do
+    describe service('zabbix-agent') do
+        it { should be_enabled }
+        it { should be_running }
+    end
+
+    describe port(10050) do
+        it { should be_listening }
+    end
+end
+
 describe 'Zabbix Agent Configuration' do
     describe file('/etc/zabbix/zabbix_agent.conf') do
         it { should be_file}
         it { should be_owned_by 'zabbix'}
         it { should be_grouped_into 'zabbix'}
+
+        it { should contain "ListenPort==10050" }
+        it { should contain "Server=192.168.3.33" }
+        it { should contain "ServerActive=192.168.3.33" }
     end
 end
