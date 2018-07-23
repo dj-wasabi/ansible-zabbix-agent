13476d2fe145b8725fd3f3686491e9bc797d47b6
Werner Dijkerman <werner@dj-wasabi.nl>
2018-07-23 20:57:46 +0200

    Added 2nd Molecule Scenario

---
+++ molecule/with-server/tests/test_default.py
@@ -0,0 +1,43 @@
+import os
+import pytest
+from zabbix_api import ZabbixAPI
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('zabbix_server')
+
+
+@pytest.fixture
+def authenticate():
+    zapi = ZabbixAPI(server='http://zabbix-server-centos/api_jsonrpc.php')
+    zapi.login("Admin", "zabbix")
+    return zapi
+
+
+@pytest.fixture
+def get_hosts():
+    return [
+        "zabbix-agent-debian",
+        "zabbix-agent-ubuntu",
+        "zabbix-agent-centos"
+    ]
+
+
+def test_hosts():
+    zapi = authenticate()
+    hosts = get_hosts()
+    servers = zapi.host.get({'output': ["hostid", "name"]})
+
+    for server in servers:
+        if server['name'] != 'Zabbix server':
+            assert server['name'] in hosts
+
+
+def test_hosts_status():
+    zapi = authenticate()
+    servers = zapi.host.get({'output': ["status", "name"]})
+
+    for server in servers:
+        if server['name'] != 'Zabbix server':
+            assert int(server['status']) == 0
